751ac9a54b459738b16b3f852a56cb369e4a3d5e
David Arnold <dar@devco.co>
2015-07-17 11:28:11 -0500

    cleared unnecessary file content

--- watch_windows.go
+++ watch_windows.go
@@ -2,32 +2,7 @@ package main
 
 import (
 	"golang.org/x/exp/winfsnotify" "wfsevent"
-	"sort"
-	"time"
 )
-
-var noteDescription = map[fsevents.EventFlags]string{
-	fsevents.MustScanSubDirs: "MustScanSubdirs",
-	fsevents.UserDropped:     "UserDropped",
-	fsevents.KernelDropped:   "KernelDropped",
-	fsevents.EventIDsWrapped: "EventIDsWrapped",
-	fsevents.HistoryDone:     "HistoryDone",
-	fsevents.RootChanged:     "RootChanged",
-	fsevents.Mount:           "Mount",
-	fsevents.Unmount:         "Unmount",
-
-	fsevents.ItemCreated:       "Created",
-	fsevents.ItemRemoved:       "Removed",
-	fsevents.ItemInodeMetaMod:  "InodeMetaMod",
-	fsevents.ItemRenamed:       "Renamed",
-	fsevents.ItemModified:      "Modified",
-	fsevents.ItemFinderInfoMod: "FinderInfoMod",
-	fsevents.ItemChangeOwner:   "ChangeOwner",
-	fsevents.ItemXattrMod:      "XAttrMod",
-	fsevents.ItemIsFile:        "IsFile",
-	fsevents.ItemIsDir:         "IsDir",
-	fsevents.ItemIsSymlink:     "IsSymLink",
-}
 
 func Watch(path string, eventHandler func(id uint64, path string, flags []string)) {
 
