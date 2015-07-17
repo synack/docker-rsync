671577938432541143c172b70c1ce82e71bd9437
David Arnold <dar@devco.co>
2015-07-17 11:24:19 -0500

    Pseudocode for Windows Notification Implementation

---
+++ watch_windows.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"golang.org/x/exp/winfsnotify" "wfsevent"
+	"sort"
+	"time"
+)
+
+var noteDescription = map[fsevents.EventFlags]string{
+	fsevents.MustScanSubDirs: "MustScanSubdirs",
+	fsevents.UserDropped:     "UserDropped",
+	fsevents.KernelDropped:   "KernelDropped",
+	fsevents.EventIDsWrapped: "EventIDsWrapped",
+	fsevents.HistoryDone:     "HistoryDone",
+	fsevents.RootChanged:     "RootChanged",
+	fsevents.Mount:           "Mount",
+	fsevents.Unmount:         "Unmount",
+
+	fsevents.ItemCreated:       "Created",
+	fsevents.ItemRemoved:       "Removed",
+	fsevents.ItemInodeMetaMod:  "InodeMetaMod",
+	fsevents.ItemRenamed:       "Renamed",
+	fsevents.ItemModified:      "Modified",
+	fsevents.ItemFinderInfoMod: "FinderInfoMod",
+	fsevents.ItemChangeOwner:   "ChangeOwner",
+	fsevents.ItemXattrMod:      "XAttrMod",
+	fsevents.ItemIsFile:        "IsFile",
+	fsevents.ItemIsDir:         "IsDir",
+	fsevents.ItemIsSymlink:     "IsSymLink",
+}
+
+func Watch(path string, eventHandler func(id uint64, path string, flags []string)) {
+
+	
+	es := &wfsevent.NewWatcher.Watch{[]string{path}}
+	ec := es.Event.String
+
+	for {
+		select {
+		case event := <-ec:
+			go eventHandler(1 uint64, "dummy", [0])
+		}
+	}
+}
